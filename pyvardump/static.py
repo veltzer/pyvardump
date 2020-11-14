25af3aa47ded0dc0d9ea2c54e61645897d75f0e9
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-14 09:49:16 +0200

    next version

--- pyvardump/static.py
+++ pyvardump/static.py
@@ -1,4 +1,4 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.1"
+VERSION_STR = "0.0.2"
 DESCRIPTION = "pyvardump helps you dump variables in python"
 APP_NAME = "pyvardump"
