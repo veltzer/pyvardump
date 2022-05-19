1987164eba584595f3149102eaa5a1fee7779089
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-19 19:12:44 +0300

    new config

--- config/python.py
+++ config/python.py
@@ -1,7 +1,3 @@
-import config.project
-
-package_name = config.project.project_name
-
 dev_requires = [
     "pyclassifiers",
     "pypitools",
@@ -16,8 +12,3 @@ test_requires = [
     "flake8",
     "pymakehelper",
 ]
-
-python_requires = ">=3.10"
-
-test_os = ["ubuntu-22.04"]
-test_python = ["3.10"]
