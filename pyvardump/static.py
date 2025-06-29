0709a31d57399595580b7709afb482b02fe01d42
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:34:02 +0300

    fixed up deps

--- pyvardump/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.5"
-DESCRIPTION = "pyvardump helps you dump variables in python"
-APP_NAME = "pyvardump"
-LOGGER_NAME = "pyvardump"
